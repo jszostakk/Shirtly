46fdd84d209bfe735acd124542daf9e23b38679d
Jakub Szostak <jakub.szostakk@gmail.com>
2023-03-28 16:14:03 +0200

    test controller

---
+++ routes/api.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const testController = require('../controllers/api/test');
+
+router.get('/', testController.homepage);
+
+module.exports = router;
