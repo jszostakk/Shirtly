4b62d2b0c157d1f4123d59a20d7842c99a9702bc
Jakub Szostak <jakub.szostakk@gmail.com>
2023-05-31 02:38:51 +0200

    Some css changes

--- frontend/src/components/entry/Entry.js
+++ frontend/src/components/entry/Entry.js
@@ -1,38 +1,39 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './Entry.css';
 import '../../../../node_modules/bootstrap/dist/css/bootstrap.css';
 
 function Entry(props) {
-
     const [showDesc, setShowDesc] = useState(false);
 
     const toggleDesc = () => {
         setShowDesc(!showDesc);
-    }
+    };
+
     const editHandler = () => {
         props.onEdit({
             title: props.title,
-            description:props.description,
-            _id:props.id});
-    }
+            description: props.description,
+            _id: props.id,
+        });
+    };
 
     return (
         <a className="article">
             <div className="titleDescription">
                 <h1 onClick={toggleDesc}>{props.title}</h1>
-                {showDesc && (<p>
-                    {props.description}
-                </p>)}
-
+                {showDesc && <p>{props.description}</p>}
             </div>
-            <button className="delete" onClick={() => props.onDelete(props.id)}>
-                usun
-            </button>
-            <button className="edit" onClick={editHandler}>
-                edytuj
-            </button>
+
+            <div className="buttonGroup">
+                <button className="delete" onClick={() => props.onDelete(props.id)}>
+                    usun
+                </button>
+                <button className="edit" onClick={editHandler}>
+                    edytuj
+                </button>
+            </div>
         </a>
-    )
+    );
 }
 
 export default Entry;
